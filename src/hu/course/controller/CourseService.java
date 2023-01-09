c2ca40add39e265ac6979f941e59c4087ec024d0
ujvarige19 <ujvari.gergo01@gmail.com>
2023-01-09 09:04:18 +0100

    Full Task

--- src/hu/course/controller/CourseService.java
+++ src/hu/course/controller/CourseService.java
@@ -13,4 +13,51 @@ public class CourseService {
         this.courses = courses;
     }
 
+    public int getCourseCount(){
+        return courses.size();
+    }
+
+    public int getTotalLessonCount(){
+        return courses.stream()
+                .mapToInt(Course::getWeeklyLessonNumber)
+                .sum();
+    }
+
+    public int getTeacherWeeklyLesson(String teacher) {
+        return courses.stream()
+                .filter(i -> i.isTeacher(teacher))
+                .mapToInt(Course::getWeeklyLessonNumber)
+                .sum();
+    }
+
+    public List<String> getMasterTeachers(){
+        return courses.stream()
+                .filter(i ->i.isSubject("osztalyfonoki"))
+                .map(Course::toString)
+                .collect(Collectors.toList());
+    }
+
+    public String getCourseStatus(String classId, String subject){
+        long count = getCertainCourseCount(classId, subject);
+        if (count == 0) {
+            return "Nem ";
+        } else {
+            return count == 1 ? "Osztályszinten" : "Csoportbontásban";
+        }
+    }
+
+    private long getCertainCourseCount(String classId, String subject){
+        return courses.stream()
+                .filter(i ->i.isClassAndSubject(classId, subject))
+                .count();
+    }
+
+    public long getTeachersCount(){
+        return courses.stream()
+                .map(Course::getTeacher)
+                .distinct()
+                .count();
+
+    }
+
 }
